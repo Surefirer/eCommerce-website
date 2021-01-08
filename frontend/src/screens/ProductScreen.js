384593c4e1205dd38eb83840af2597e0b63076b0
Surefirer <46170182+Surefirer@users.noreply.github.com>
2021-01-07 19:13:42 -0800

    adjust product detial page layout

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -78,27 +78,23 @@ const ProductScreen = ({ history, match }) => {
         <>
           <Meta title={product.name} />
           <Row>
-            <Col md={6}>
+            <Col className='pb-4' md={8}>
               <Image src={product.image} alt={product.name} fluid />
             </Col>
-            <Col md={3}>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    value={product.rating}
-                    text={`$${product.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {product.description}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-            <Col md={3}>
+            <Col md={4}>
+              <Card>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Rating
+                      value={product.rating}
+                      text={`$${product.numReviews} reviews`}
+                    />
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
               <Card>
                 <ListGroup variant='flush'>
                   <ListGroup.Item>
@@ -156,6 +152,17 @@ const ProductScreen = ({ history, match }) => {
                   </ListGroup.Item>
                 </ListGroup>
               </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <h1 className='font-weight-bold'>Description:</h1>{' '}
+                  {product.description}
+                </ListGroup.Item>
+              </ListGroup>
             </Col>
           </Row>
 
